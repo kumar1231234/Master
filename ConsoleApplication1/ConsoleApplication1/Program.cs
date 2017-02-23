600e42f90bb02867b405b633a858dbe3131d0336
vikash_1231234 <vikashkumarraja09@gmail.com>
2017-02-23 19:32:04 +0530

    Post Request using Http Client.

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
 
 namespace ConsoleApplication1
 {
@@ -6,7 +9,35 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            //PostRequest("https://posttestserver.com/post.php");
+
+            PostRequest("https://posttestserver.com/post.php");
             Console.ReadLine();
+        }
+
+        async static void PostRequest(string url)
+        {
+            IEnumerable<KeyValuePair<string, string>> quaries = new List<KeyValuePair<string, string>>()
+            {
+                new KeyValuePair<string, string>("query1","jamal"),
+            new KeyValuePair<string, string>("query2", "hussain")
+            };
+            HttpContent q = new FormUrlEncodedContent(quaries);
+
+            using (HttpClient httpclient = new HttpClient())
+            {
+                using (HttpResponseMessage httpResponseMsg = await httpclient.PostAsync(url, q))
+                {
+                    using (HttpContent content = httpResponseMsg.Content)
+                    {
+                        string mycontent = await content.ReadAsStringAsync();
+                        HttpContentHeaders headers = content.Headers;
+                        Console.WriteLine(mycontent);
+
+                    }
+                }
+
+            }
         }
     }
 }
